d5bba266dde332a1c6b2ea6e1ae636abb13d3646
Rory <Rory@Snowfall>
2014-06-06 21:26:12 -0700

    More

--- src/entities/Wall.java
+++ src/entities/Wall.java
@@ -1,6 +1,7 @@
 package entities;
 
 import collisions.Vector;
+import graphics.Graphics;
 
 public class Wall extends Solid {
 
@@ -8,6 +9,18 @@ public class Wall extends Solid {
         super(pos);
         bounds.size = new Vector(16, 16, 16);
         setSprite("Wall");
+    }
+    
+    @Override
+    public void draw() {
+        //Top
+        Graphics.drawSprite(sprite, pos.x, pos.y, pos.z + 16, (int) Math.round(imageIndex), 0);
+        //Bottom
+        Graphics.drawSprite(sprite, pos.x, pos.y, pos.z - 16, (int) Math.round(imageIndex), 0);
+        
+        //Sides
+        Graphics.drawSprite(sprite, pos.x, pos.y, pos.z, (int) Math.round(imageIndex), 0);
+        Graphics.drawSprite(sprite, pos.x, pos.y - 16, pos.z, (int) Math.round(imageIndex), 0);
     }
 
 }
